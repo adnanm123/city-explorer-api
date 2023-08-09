1a7eb32ac1f380d6637a40b737bba2a27f996197
Adnan Mohamud <adnanmohamud6@gmail.com>
2023-08-09 15:23:56 -0700

    added in the weatherbit data

--- server.js
+++ server.js
@@ -1,44 +1,47 @@
 "use strict";
 
+// REQUIRE
+// required from npm
 const express = require("express");
-
 require("dotenv").config();
-
-let data = require("./data/weather.json");
-
 const cors = require("cors");
+const axios = require("axios");
 
+// instantiate express server by calling express
 const app = express();
 
+// USE
 app.use(cors());
 
+// Define port and proof that env works
 const PORT = process.env.PORT || 3002;
 
-app.get("/weather", handleWeather);
-
-app.get("/", (request, response) => {
-  response.send("hello, from our server");
-});
-function handleWeather(request, response) {
+// ROUTES
+app.get("/weather", async (request, response) => {
   let lat = request.query.lat;
   let lon = request.query.lon;
+  let weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHERBITAPI_SERVER_URL}&lat=${lat}&lon=${lon}&units=I&days=5`;
+  let weatherBitData = await axios.get(weatherBitUrl);
+  console.log(weatherBitData);
 
-  const latData = data.find(
-    (location) => location.lat === lat && location.lon === lon
-  );
   try {
-    const weatherArray = latData.data.map((day) => new Forecast(day));
+    const weatherArray = weatherBitData.data.data.map((day) => new Forecast(day));
     response.status(200).send(weatherArray);
   } catch (error) {
     response.status(500).send("something went wrong");
     console.log(error.message);
   }
-}
+});
+
+app.get("/", (request, response) => {
+  response.send("hello, from our server");
+});
 
 app.get("*", (request, response) => {
   response.send("The thing you are looking for doesn't exist");
 });
 
+// CLASSES
 class Forecast {
   constructor(day) {
     console.log("each day object", day);
@@ -49,8 +52,11 @@ class Forecast {
   }
 }
 
+// ERRORS
 app.use((error, request, response, next) => {
   response.status(500).send(error.message);
 });
 
+// Listen
+// need to listen to keep server running
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
